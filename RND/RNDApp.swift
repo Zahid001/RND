c83620211be4613bd03cc2de4a9191e0f0bbb155
Md Zahidul Islam Mazumder <zahidul.mazumder@genexinfosys.com>
2025-04-24 02:07:25 +0600

    Initial commit: Set up project

---
+++ RND/RNDApp.swift
@@ -0,0 +1,23 @@
+//
+//  RNDApp.swift
+//  RND
+//
+//  Created by Md Zahidul Islam Mazumder on 4/22/25.
+//
+
+import SwiftUI
+import Firebase
+
+@main
+struct RNDApp: App {
+    
+    init(){
+        FirebaseApp.configure()
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            EmptyView()
+        }
+    }
+}
---
+++ RNDTests/RNDTests.swift
@@ -0,0 +1,19 @@
+//
+//  RNDTests.swift
+//  RNDTests
+//
+//  Created by Md Zahidul Islam Mazumder on 4/24/25.
+//
+
+import Testing
+import XCTest
+import RealmSwift
+@testable import RND
+
+struct RNDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
